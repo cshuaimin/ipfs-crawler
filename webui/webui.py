c2a3f4c6b352b99ff2e1cf1fa2f1b4d8146bdcdc
Chen Shuaimin <chen_shuaimin@outlook.com>
2018-08-12 17:15:13 +0800

    search using pg

---
+++ webui/webui.py
@@ -0,0 +1,42 @@
+import aiohttp_jinja2
+import jinja2
+import asyncpg
+from aiohttp import web
+
+
+@aiohttp_jinja2.template('index.jinja2')
+def index(request):
+    return {}
+
+
+@aiohttp_jinja2.template('results.jinja2')
+async def search(request):
+    query = request.query.get('q')
+    if not query:
+        raise web.HTTPFound('/')
+    result = await request.app['pool'].fetch(
+        '''SELECT hash, title, text, ts_rank_cd(tsv, query) AS rank
+         FROM crawler_html, to_tsquery($1) query
+         WHERE tsv @@ query
+         ORDER BY rank DESC
+         LIMIT 10 ''',
+        query
+    )
+    return {'result': result}
+
+
+app = web.Application()
+aiohttp_jinja2.setup(
+    app,
+    loader=jinja2.PackageLoader('webui', 'templates')
+)
+app.router.add_get('/', index)
+app.router.add_get('/search', search)
+app.router.add_static('/static/', '/home/csm/code/ipfs_crawler/webui/static')
+async def conn_pool(app):
+    app['pool'] = await asyncpg.create_pool(user='postgres', database='ipfs_crawler')
+    yield
+    await app['pool'].close()
+app.cleanup_ctx.append(conn_pool)
+web.run_app(app)
+
